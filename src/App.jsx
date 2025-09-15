658ea3922ad621425f4e8d13693e7c533ee049f3
Hor Tengmeng <tengmeng382@gmail.com>
2025-09-15 16:30:31 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./Home";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl underline text-pink-500">
+      Hello world!
+      <Home/>
+    </h1>
+  )                                                   
+}
+
+
+
+
+
+
